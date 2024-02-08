712e400d524376811354296c3bd3659540860c79
Votre Nom <votre@email.com>
2024-02-08 17:58:15 +0100

    avec nouvelle icone

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,10 +1,8 @@
-<script>
-</script>
-
 <footer>
-    <ul>
+    <ul class= "footerPages">
         <li>Conditions d'utilisation</li>
         <li>Confidentialité et gestion des cookies</li>
     </ul>
     <p>O'Brunch 2024 - Tous droits réservés</p>
 </footer>
+
