27eb53d45e46d0b06dbb3f86a3be8287fcd9401e
JEREMYRONCEHENDRIKS <jroncedev@laposte.net>
2024-02-08 14:35:59 +0100

    ajout composants et styles scss

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,11 +1,17 @@
 <script>
 
-
-
 </script>
 
 <header>
 
 <img class="logo" src="/public/images/logo.svg" alt = "logo" />
+
+<nav>
+    <ul>
+            <li>A propos</li>
+            <li>Historique</li>
+            <li>Connexion</li>
+    </ul>
+</nav>
 
 </header>
