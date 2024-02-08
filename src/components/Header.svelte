12e41ba10e955a4b80852b34b4d966dbc50e337e
JEREMYRONCEHENDRIKS <jroncedev@laposte.net>
2024-02-08 17:58:15 +0100

    avec nouvelle icone

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,17 +1,20 @@
-<script>
-
-</script>
-
 <header>
+    <img class="logo" src="/public/images/logo.svg" alt="logo" />
 
-<img class="logo" src="/public/images/logo.svg" alt = "logo" />
-
-<nav>
-    <ul>
+    <nav class="header_nav">
+        <ul>
             <li>A propos</li>
             <li>Historique</li>
-            <li>Connexion</li>
-    </ul>
-</nav>
+        </ul>
+    </nav>
+
+<div class="connexionSection">
+    <img class="connexionIcon" src="/public/images/icone_connexion.jpg" alt="logo" />
+    <p>CONNEXION</p>
+</div>
 
 </header>
+
+
+
+
