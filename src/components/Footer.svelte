bb8f2d0065f9ba4935c42d523ae3f30a15d36733
Votre Nom <votre@email.com>
2024-02-08 14:35:59 +0100

    ajout composants et styles scss

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,3 +1,10 @@
+<script>
+</script>
+
 <footer>
+    <ul>
+        <li>Conditions d'utilisation</li>
+        <li>Confidentialité et gestion des cookies</li>
+    </ul>
     <p>O'Brunch 2024 - Tous droits réservés</p>
 </footer>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import './app.css'
+import './style/main.scss'
 import App from './App.svelte'
 
 const app = new App({
