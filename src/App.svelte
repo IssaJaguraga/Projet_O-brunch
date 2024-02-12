352492cd63b9295325e7d278441db983ab72d33e
IssaJaguraga <issa.jaguraga@oclock.school>
2024-02-12 20:03:16 +0100

    All restaurants

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,11 @@
 <script>
 
 import Header from "./components/Header.svelte";
+import AllRestaurant from "./pages/AllRestaurant.svelte";
 import Footer from "./components/Footer.svelte";
 
 </script>
 
 <Header />
-
+<AllRestaurant />
 <Footer />
---
+++ src/pages/AllRestaurant.svelte
@@ -0,0 +1,76 @@
+<main>
+
+<h1>Les meuilleurs brunch de Dubaï</h1>
+
+<h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae doloremque qui excepturi nesciunt consequuntur ut distinctio, sit sequi eveniet corrupti, necessitatibus nemo maiores dolore odit eaque. Consequatur facilis neque impedit?</h2>
+
+
+
+ <section class="AllRestaurant" aria-label="les salles de restaurant">
+   
+   <article class="theme-card">
+
+     <div class="theme-card__informations">
+        <span>restaurant Nara</span>
+     </div>
+     <a
+       href="/#"
+       class="theme-card__button"
+       aria-label="Accéder à l'espace restaurant">Voir le restaurant 1</a>
+   </article>
+
+ 
+   <article class="theme-card">
+     <div class="theme-card__informations">
+      
+           <span>restaurant safran</span>
+       
+     </div>
+     <a
+       href="/2"
+       class="theme-card__button"
+       aria-label="Accéder à l'espace restaurant">Voir le restaurant 2</a>
+   </article>
+
+   <article class="theme-card">
+    <div class="theme-card__informations">
+      
+       
+          <span>restaurant Mina's</span>
+      
+    </div>
+    <a
+      href="/3"
+      class="theme-card__button"
+      aria-label="Accéder à l'espace restaurant">Voir le restaurant 3</a>
+  </article>
+
+  <article class="theme-card">
+    <div class="theme-card__informations">
+      
+          <span>restaurant Musk</span>
+     
+    </div>
+    <a
+      href="/4"
+      class="theme-card__button"
+      aria-label="Accéder à l'espace restaurant">Voir le restaurant 4</a>
+  </article>
+
+   
+  <article class="theme-card">
+    <div class="theme-card__informations">
+      
+       
+          <span>restaurant La Turca</span>
+      
+    </div>
+    <a
+      href="/5"
+      class="theme-card__button"
+      aria-label="Accéder à l'espace restaurant">Voir le restaurant 5</a>
+  </article>
+ 
+  
+ </section>
+</main>
