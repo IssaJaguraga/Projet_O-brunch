985ad00f0ad64fdc1a0a1d5266e8db75b39b1ca6
OuattaraMariam <mariamouattara070@gmail.com>
2024-02-13 17:03:03 +0100

    condition&rgpd

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,10 @@
 
 import Header from "./components/Header.svelte";
 import Footer from "./components/Footer.svelte";
+import Conditions from "./pages/Conditions.svelte";
 
 </script>
 
 <Header />
-
+<Conditions />
 <Footer />
---
+++ src/pages/rgpd.svelte
@@ -0,0 +1,17 @@
+<main>
+    <section class="privacy-policy">
+        <h1>Politique de confidentialité</h1>
+            <p>Cette Politique de confidentialité décrit les types d'informations que nous collectons sur les utilisateurs, la manière dont nous utilisons ces informations et avec qui nous les partageons. Nous respectons la vie privée de nos utilisateurs et nous nous engageons à protéger les informations personnelles que vous partagez avec nous.</p>
+                <h2>Informations collectées</h2>
+                    <p>Nous collectons des informations personnelles telles que votre nom, votre adresse e-mail et d'autres informations que vous nous fournissez volontairement lorsque vous utilisez notre site Web.</p>
+                <h2>Utilisation des informations</h2>
+                    <p>Nous utilisons les informations que nous collectons pour améliorer notre site Web, personnaliser votre expérience utilisateur et vous envoyer des communications marketing si vous avez donné votre consentement à cet effet.</p>
+                <h2>Partage des informations</h2>
+                    <p>Nous ne vendons pas, n'échangeons pas et ne louons pas vos informations personnelles à des tiers sans votre consentement explicite. Nous pouvons partager vos informations avec des tiers de confiance qui nous aident à exploiter notre site Web ou à mener nos activités, à condition qu'ils acceptent de garder ces informations confidentielles.</p>
+                <h2>Cookies</h2>
+                    <p>Notre site Web utilise des cookies pour améliorer votre expérience utilisateur. En continuant à utiliser notre site, vous consentez à notre utilisation de cookies conformément à notre Politique de confidentialité.</p>
+                <h2>Consentement</h2>
+                    <p>En utilisant notre site Web, vous consentez à notre Politique de confidentialité et à nos conditions d'utilisation.</p>
+    </section>
+
+</main>
