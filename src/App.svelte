c04d6c7e6557292070c5a9ff371c887c8c6ff2e0
Votre Nom <votre@email.com>
2024-02-14 14:54:31 +0100

    Login and register

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,13 @@
 <script>
 
 import Header from "./components/Header.svelte";
-import Login from "./pages/Login.svelte";
-
 import Footer from "./components/Footer.svelte";
+import Login from "./pages/Login.svelte";
+import Register from "./pages/Register.svelte";
+import About from "./pages/about.svelte";
+import History from "./pages/history.svelte";
+
+
 //import Router, { location } from "svelte-spa-router";
 //import routes from "./routes";
 
