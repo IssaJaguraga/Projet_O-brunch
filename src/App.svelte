2fc81c17db3452408d8ce704fea2308921f9c887
JEREMYRONCEHENDRIKS <jroncedev@laposte.net>
2024-02-14 14:17:43 +0100

    formulaire

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,20 @@
 <script>
 
 import Header from "./components/Header.svelte";
-// import  history & about
-import History from "./pages/history.html";
-import About from "./pages/about.html";
+import Login from "./pages/Login.svelte";
+
 import Footer from "./components/Footer.svelte";
 //import Router, { location } from "svelte-spa-router";
 //import routes from "./routes";
 
-
 </script>
 
 
-<Header/>
-<Footer/>
+<Header />
+<Login />
+<Footer />
+
+
 
 
 
---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import About from "./pages/about.svelte";
+import History from "./pages/history.svelte";
+import Login from "./pages/Login.svelte";
+import Register from "./pages/Register.svelte";
+
+
+export default {
+  "/": Home,
+  "/about": About,
+  "/history": History,
+  "/login": Login,
+  "/register": Register,
+};
