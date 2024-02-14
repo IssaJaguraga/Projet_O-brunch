e7f1e00c6002bd5e60298af68fc1680162bda294
GregoryDovy <gregory.dovy-popotte@oclock.school>
2024-02-14 02:42:02 +0100

    ajout de la page type restaurant

--- src/App.svelte
+++ src/App.svelte
@@ -2,14 +2,13 @@
 
 import Header from "./components/Header.svelte";
 import Footer from "./components/Footer.svelte";
-<<<<<<< HEAD
-import Main from "./components/Main.svelte";
-=======
+
+
 
 //import Router, { location } from "svelte-spa-router";
 //import routes from "./routes";
 
->>>>>>> origin/dev
+
 </script>
 
 
