a23784ada79cf9d33c99b32fb0d4d8e435929710
JEREMYRONCEHENDRIKS <jroncedev@laposte.net>
2024-02-14 17:16:56 +0100

    avec routes

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import About from "./pages/about.svelte";
 import History from "./pages/history.svelte";
 import Login from "./pages/Login.svelte";
 import Register from "./pages/Register.svelte";
+import Home from "./pages/Home.svelte";
 
 
 export default {
