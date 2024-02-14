e3101dd4c692595fdd05f3537a7df101bd991342
Votre Nom <votre@email.com>
2024-02-14 02:26:53 +0100

    pages about&history

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,8 @@
 import Header from "./components/Header.svelte";
 import Footer from "./components/Footer.svelte";
 
+import History from "./pages/history.svelte";
+import About from "./pages/about.svelte";
 </script>
 
 <Header />
