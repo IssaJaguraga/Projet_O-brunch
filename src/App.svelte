93564bffccf85fe13413e774be0437191e2253bc
s3bastien-4ndy <v3ndred1@gmail.com>
2024-02-16 09:57:12 +0100

    correction

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,6 @@
 
 import Header from "./components/Header.svelte";
 import Footer from "./components/Footer.svelte";
-import Conditions from "./pages/Conditions.svelte";
 
 import Router, {location} from "svelte-spa-router";
 import routes from "./routes";
@@ -15,9 +14,6 @@ beforeUpdate(() => {
   });
 </script>
 
-<Header />
-<Conditions />
-<Footer />
 
 
 
