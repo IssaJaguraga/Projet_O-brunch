1f9be9a8c8c390b7d4ed974036e3ce2f7dfd6a0a
JEREMYRONCEHENDRIKS <jroncedev@laposte.net>
2024-02-20 14:02:28 +0100

    add link footer

--- src/App.svelte
+++ src/App.svelte
@@ -2,53 +2,24 @@
 
 import Header from "./components/Header.svelte";
 import Footer from "./components/Footer.svelte";
-import PageType from "./components/PageType.svelte";
-
-
-
 import Router, {location} from "svelte-spa-router";
 import routes from "./routes";
-import { beforeUpdate } from "svelte";
 
 
-beforeUpdate(() => {
-    $location === "/"
-      ? document.body.classList.add("homepage")
-      : document.body.classList.remove("homepage");
-  });
-
-
-
-<<<<<<< Updated upstream
 </script>
 
 
 
-
-
-
-<Header />
-
-
-<Router {routes} />
-<Footer />
-
-
-
-
-
-
-
-=======
-import Router, {location} from "svelte-spa-router";
-import routes from "./routes";
-
-</script>
-
 <Header />
 <Router {routes} />
 <Footer />
->>>>>>> Stashed changes
+
+
+
+
+
+
+
 
 
 
